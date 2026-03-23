b51489f74b6157bd42d69db24a2eea4395791b40
ALUMNO2425 <ALUMNO2425@DESKTOP-CDGKK4P>
2026-03-23 10:33:30 +0100

    Cambio a la función principal

--- Github_Prueba/src/entornos/desarrollo/App.java
+++ Github_Prueba/src/entornos/desarrollo/App.java
@@ -3,7 +3,7 @@ package entornos.desarrollo;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated mejjjhjhjhjhthod stub
 
 	}
 
